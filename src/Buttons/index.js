52e04c95afca84d28a399ba008ad163855b664c9
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-07-03 16:54:28 -0700

    Add sizes to buttons

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,6 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Body1, Body2, Body3 } from '../Typography.js';
 
-const Button = ({children, ...props}) =>
-    <button {...props}>{children}</button>
+const getHeight = ({ size }) => {
+    const sizeMap = {
+        sm: '2.4rem',
+        md: '4rem',
+        lg: '4.8rem'
+    }
+    return sizeMap[size];
+}
+
+const StyledButton = styled.button`
+    font-family: inherit;
+    height: ${getHeight};
+    border: 0;
+    background: #009E8F;
+    border-radius: 0.4rem;
+    padding: 0 1.6rem;
+    cursor: pointer;
+`;
+
+const Button = ({ children, ...restProps }) => {
+    const textMap = {
+        'sm': Body3,
+        'md': Body2,
+        'lg': Body1,
+    };
+    const Typography = textMap[restProps.size];
+
+    return (
+        <StyledButton {...restProps}>
+            <Typography color="contrast" weight="bold">{children}</Typography>
+        </StyledButton>
+    )
+};
+
+Button.defaultProps = {
+    'size': 'md'
+}
 
 export default Button;
