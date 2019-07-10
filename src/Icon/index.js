a333f5f8e6ea25649eee4ff388ed5912797b7aac
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-07-10 15:42:43 -0700

    Throw rest props

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 const files = require.context('./icons', false, /.*\.svg$/);
 files.keys().forEach(files);
 
-const Icon = ({ type, className }) => {
-  return (<svg width="1em" height="1em">
+const Icon = ({ type, size, ...restProps }) => {
+  return (<svg width={`${size}rem`} height={`${size}rem`} {...restProps}>
     <use xlinkHref={ `#${ type }` }></use>
   </svg>);
 };
+
+Icon.defaultProps = {
+    size: 3.2
+}
 
 export default Icon;
