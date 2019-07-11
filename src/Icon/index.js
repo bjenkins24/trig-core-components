b59791656a7200bbc3334bb173b5e0c67bab5756
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-07-10 17:58:54 -0700

    Eslint fixes

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,17 +1,24 @@
-import React from "react";
-const files = require.context("./icons", false, /.*\.svg$/);
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const files = require.context('./icons', false, /.*\.svg$/);
 files.keys().forEach(files);
 
 const Icon = ({ type, size, ...restProps }) => {
-    return (
-        <svg width={`${size}rem`} height={`${size}rem`} {...restProps}>
-            <use xlinkHref={`#${type}`}></use>
-        </svg>
-    );
+  return (
+    <svg width={`${size}rem`} height={`${size}rem`} {...restProps}>
+      <use xlinkHref={`#${type}`} />
+    </svg>
+  );
 };
 
 Icon.defaultProps = {
-    size: 3.2
+  size: 3.2,
+};
+
+Icon.propTypes = {
+  type: PropTypes.string.isRequired,
+  size: PropTypes.number,
 };
 
 export default Icon;
