756f9ed2c78471a8447bb2ab08fe7c8f155bf0c5
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-10-25 21:01:42 -0700

    Move tabs out to it's own story file

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,13 +2,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import {
-  withKnobs,
-  select,
-  number,
-  text,
-  boolean,
-} from '@storybook/addon-knobs';
+import { withKnobs, select, number, text } from '@storybook/addon-knobs';
 
 import { ThemeProvider } from 'styled-components';
 import './consoleOverrides';
@@ -18,7 +12,6 @@ import Fab from '../src/Buttons/Fab';
 import ButtonSelect from '../src/Buttons/ButtonSelect';
 import Icon from '../src/Icon';
 import Avatar from '../src/Avatar';
-import { Tabs, TabList, Tab, TabPanel } from '../src/Tabs';
 import Popover from '../src/Popover';
 import Deck from '../src/Deck';
 import {
@@ -111,26 +104,6 @@ storiesOf('Button', module)
       </ButtonToggle>
     );
   });
-
-storiesOf('Tabs', module)
-  .addDecorator((story) => (
-    <ThemeProvider theme={theme}>{story()}</ThemeProvider>
-  ))
-  .addDecorator(withKnobs)
-  .add('default', () => (
-    <Tabs>
-      <TabList dark={boolean('Dark', false)}>
-        <Tab>All Cards</Tab>
-        <Tab>Recently Viewed</Tab>
-        <Tab>Most Viewed</Tab>
-        <Tab>Favorites</Tab>
-      </TabList>
-      <TabPanel>All Cards Stuff</TabPanel>
-      <TabPanel>Recently Viewed Stuff</TabPanel>
-      <TabPanel>Most Viewed Stuff</TabPanel>
-      <TabPanel>Favorite Stuff</TabPanel>
-    </Tabs>
-  ));
 
 storiesOf('Popover', module)
   .addDecorator((story) => (
