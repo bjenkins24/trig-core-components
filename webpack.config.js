8c8e902ab54c0bc6954b5fecb8c1f54c2ad5c3f4
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-11-21 17:43:55 -0800

    Spell it right

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
     library: '@trig-app/core-components',
-    librayTarget: 'umd',
+    libraryTarget: 'umd',
     globalObject: 'this',
   },
   plugins: [new BundleAnalyzerPlugin()],
