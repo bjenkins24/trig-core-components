46c72759ff173465b082b7820d3e3d5841868480
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-12-01 15:29:05 -0800

    Allow adding custom theme

--- src/test/utils.js
+++ src/test/utils.js
@@ -2,17 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import { render as rtlRender } from '@testing-library/react';
-import theme from '../../stories/theme';
+import themes from '../../stories/theme';
 
-const Wrapper = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-};
+const render = (ui, { theme = themes, ...options }) => {
+  const Wrapper = ({ children }) => {
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  };
 
-Wrapper.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-const render = (ui, options) => {
+  Wrapper.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
   return rtlRender(ui, { wrapper: Wrapper, ...options });
 };
 
