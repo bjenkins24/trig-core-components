f8426338020600cc0732d79e6cdbacb907cdb67d
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-12-21 07:01:24 -0800

    Add coverage

---
+++ src/__tests__/Separator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'test/utils';
+import Separator from 'Separator';
+
+describe('<Separator />', () => {
+  it('renders and takes basic props', () => {
+    const { container } = render(<Separator size={3} />);
+    expect(container.firstChild).toHaveStyleRule('height', '3rem');
+  });
+});
