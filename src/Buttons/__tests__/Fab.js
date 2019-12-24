dc1c7b631b7929488c9617132973e0b0fa26b6f8
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-12-24 11:26:40 -0800

    Fab test

---
+++ src/Buttons/__tests__/Fab.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'test/utils';
+import user from '@testing-library/user-event';
+import Fab from 'Buttons/Fab';
+
+describe('<Fab />', () => {
+  it('renders and takes basic props', () => {
+    const mockCallback = jest.fn();
+    const content = 'Fab Content';
+    const { container, getByText } = render(
+      <Fab onClick={mockCallback}>{content}</Fab>
+    );
+    expect(getByText(content)).toBeInTheDocument();
+    user.click(container.firstChild);
+    expect(mockCallback.mock.calls.length).toEqual(1);
+  });
+});
